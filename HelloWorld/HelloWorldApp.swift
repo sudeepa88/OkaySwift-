22feeb317644b43d5e18610342466e01259cb5f0
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-03 19:50:17 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by sudeepa pal on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
